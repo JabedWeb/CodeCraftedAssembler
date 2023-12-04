5b809d26df8ab04a3f3db2d3a3c27e05b1ae79dc
Jabedweb <jabedhasan231@gmail.com>
2023-12-04 16:40:50 +0600

    learning variable ,input,output

---
+++ inputOuput.asm
@@ -0,0 +1,33 @@
+.model small
+.stack 100h
+.code
+
+main proc
+
+    ; Input 1
+    mov ah, 1        ; Function code for reading a character from standard input
+    int 21h          ; Call DOS interrupt to read a character
+    mov bl, al       ; Move the input character to bl register
+
+    ; Input 2
+    mov ah, 1        ; Function code for reading a character from standard input
+    int 21h          ; Call DOS interrupt to read a character
+    mov bh, al       ; Move the input character to bh register
+
+    ; Display Input 1
+    mov ah, 2        ; Function code for displaying a character
+    mov dl, bl       ; Move the character in bl to dl register for display
+    int 21h          ; Call DOS interrupt to display the character
+
+    ; Display Input 2
+    mov ah, 2        ; Function code for displaying a character
+    mov dl, bh       ; Move the character in bh to dl register for display
+    int 21h          ; Call DOS interrupt to display the character
+
+    exit : ; Exit program
+    mov ah, 4ch       ; DOS function code for program exit
+    int 21h          ; Call DOS interrupt to exit the program
+
+main endp
+
+end main
